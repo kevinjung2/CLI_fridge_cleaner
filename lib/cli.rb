679c09f9094d98e57b01124d3c4cb7ab945ff6b3
Kevin Jung <kev.jung.ii@gmail.com>
2021-02-03 18:38:27 -0500

    adds Cli class and pseudo code for it

---
+++ lib/cli.rb
@@ -0,0 +1,8 @@
+class Cli
+  #initialize a new instance that starts the program
+  #welcomes user to program and asks for ingredients that would like to be cooked
+  #gets users input and shows the user a list of recipes
+  #asks the user which recipe they would like to cook
+  #returns that recipe
+  #asks if the user would like to see the list of recipes again, put more ingredients in, or exit 
+end
