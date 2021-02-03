caf9bdc1ca42c815c6b5e0d00dfd8eca30192f30
Kevin Jung <kev.jung.ii@gmail.com>
2021-02-03 15:55:10 -0500

    added attr_accessor for all recipe attributes, also added #save and #initialize

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,8 +1,20 @@
 class Recipe
+  attr_accessor :title, :id, :used_ing, :recipe_link
+
+  @@all = []
+
+  def initialize
+    self.save
+  end
+
   def self.new_from_hash(hash)
     new = self.new
     new.title = hash["title"]
     new.id = hash["id"]
     new.used_ing = hash["usedIngredientCount"]
+  end
+
+  def save
+    @@all << self
   end
 end
