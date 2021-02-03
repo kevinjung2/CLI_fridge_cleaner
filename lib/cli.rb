19627a3e835f7ac62d911550ab2ffb4afd4522f3
Kevin Jung <kev.jung.ii@gmail.com>
2021-02-03 18:42:33 -0500

    adds #initialize and #welcome methods to Cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,8 +1,17 @@
 class Cli
   #initialize a new instance that starts the program
+  def initialize
+    self.welcome
+  end
+
+  def welcome
+    puts "Welcome to Fridge-Clear"
+    puts "Please enter ingredients that you would like to cook, one at a time. when finished enter 'done'."
+    self.get_ingredients
+  end
   #welcomes user to program and asks for ingredients that would like to be cooked
   #gets users input and shows the user a list of recipes
   #asks the user which recipe they would like to cook
   #returns that recipe
-  #asks if the user would like to see the list of recipes again, put more ingredients in, or exit 
+  #asks if the user would like to see the list of recipes again, put more ingredients in, or exit
 end
