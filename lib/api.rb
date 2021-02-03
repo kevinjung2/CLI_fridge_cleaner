23600eb7b52ce50dbae36e3c6dce0ab24c2e8bb3
Kevin Jung <kev.jung.ii@gmail.com>
2021-02-03 15:30:27 -0500

    changes #get_recipes_from_ing to a class method and populates it

--- lib/api.rb
+++ lib/api.rb
@@ -5,7 +5,9 @@ class Api
   API_KEY = "7ab46aa5f86d43f2addd9e5da1e6a23d"
 
 
-    def get_recipes_from_ing(ing_array)
-
+    def self.get_recipes_from_ing(ing_array, num_of_recipes = 2)
+      ingredients = ing_array.join(",+")
+      url = "https://api.spoonacular.com/recipes/findByIngredients?apiKey=#{API_KEY}&ingredients=#{ingredients}&number=#{num_of_recipes}"
+      response = HTTParty.get(url)
     end
 end
