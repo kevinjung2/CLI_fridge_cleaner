b238613a3c916fa6586d9f2b9653178747b2dd85
Kevin Jung <kev.jung.ii@gmail.com>
2021-02-03 19:02:29 -0500

    adds #get_ingredients to Cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -11,6 +11,33 @@ class Cli
   end
   #welcomes user to program and asks for ingredients that would like to be cooked
   #gets users input and shows the user a list of recipes
+  def get_ingredients
+    ing_array = []
+    input = gets.strip
+    while input != "done"
+      if input == "undo"
+        if ing_array.size == 0
+          begin
+            raise EmptyUndoError
+          rescue EmptyUndoError => error
+            puts EmptyUndoError.message
+          end
+        else
+          ing_array.pop
+        end
+      else
+        ing_array << input
+      end
+      puts "Please enter another ingredient, enter 'done' to finish, or enter 'undo' to undo your last entry."
+      input = gets.strip
+    end
+    if ing_array.size == 0
+      puts "You must enter at least one ingredient!"
+      self.get_ingredients
+    else
+      self.how_many(ing_array)
+    end
+  end
   #asks the user which recipe they would like to cook
   #returns that recipe
   #asks if the user would like to see the list of recipes again, put more ingredients in, or exit
