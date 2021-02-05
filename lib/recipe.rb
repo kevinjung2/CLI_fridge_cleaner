ec667c414fb812c52c3817306be6cb7db1bdd801
Kevin Jung <kev.jung.ii@gmail.com>
2021-02-05 18:00:31 -0500

    organizes the code in recipe.rb

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -5,10 +5,12 @@ class Recipe
   @@retrieved = []
   @@current = []
 
+  #saves all new instances of the Recipe class
   def initialize
     self.save
   end
 
+  #creates a new recipe instance from a hash of data retrieved by api
   def self.new_from_hash(hash)
     new = self.new
     new.title = hash["title"]
@@ -16,38 +18,52 @@ class Recipe
     new.used_ing = hash["usedIngredientCount"]
   end
 
+  #creates a number of new recipe instances from the array of hashes recieved by the api using #.new_from_hash
   def self.new_from_arr_of_hashes(arr_of_hases)
     arr_of_hases.each { |hash| self.new_from_hash(hash) }
   end
 
+  #saves an instance of a recipe to the all and current arrays
   def save
     @@all << self
     @@current << self
   end
 
+  #returns the titles of all the recipes in the @@current array
   def self.current_titles
     @@current.collect { |r| r.title }
   end
 
+  #returns the titles of all the recipes in the @@all array
   def self.all_titles
     @@all.collect { |r| r.title }
   end
 
+  #returns the titles of all the recipes in the @@retrieved array
   def self.retrieved_titles
     @@retrieved.collect { |r| r.title }
   end
 
-  def self.id_from_title(title)
-    @@all.find { |r| r.title == title }.id
-  end
-
+  # returns the @@all array
   def self.all
     @@all
   end
+
+  #returns the @@current array
+  def self.current
+    @@current
+  end
+
+  #returns the @@retrieved array
+  def self.retrieved
+    @@retrieved
+  end
+
   #add #self.find_by_id
   def self.find_by_id(id)
     @@all.find { |r| r.id == id }
   end
+
   #add #info_from_hash
   def info_from_hash(hash)
     self.recipe_link = hash["sourceUrl"]
@@ -56,11 +72,4 @@ class Recipe
     @@retrieved << self
   end
 
-  def self.current
-    @@current
-  end
-
-  def self.retrieved
-    @@retrieved
-  end
 end
